684fd83c74c96e2742ba96981f7233ff7bbbd39f
danutibe07 <danielutibe07@gmail.com>
2023-02-16 01:32:04 +0100

    app.js

--- app.js
+++ app.js
@@ -1,57 +1,159 @@
-const CardSection = document.querySelector('.cards');
-const cards = [
-  {
-    id: 'card-1',
-    title: 'Lecture',
-    imageSource: './Images/lecture1.png',
-    desc: 'Get opportunities to network with CC affilates around the world and also after the summit',
-  },
+window.addEventListener('DOMContentLoaded', () => {
+  function aboutFunction() {
+    const wrapAbout = document.getElementById('nav-wrapper-about');
+    const openWrapAbout = document.getElementById('wrapper-about');
+    const closeWrapAbout = document.getElementById('close-wrap-about');
+    function wrapFunctionA() {
+      if (wrapAbout.style.display === 'flex') {
+        wrapAbout.style.display = 'none';
+      } else {
+        wrapAbout.style.display = 'flex';
+      }
+    }
+    openWrapAbout.addEventListener('click', wrapFunctionA);
+    closeWrapAbout.addEventListener('click', wrapFunctionA);
+    const imgHomeSetup = document.querySelectorAll('.img-home-setup');
+    function homeFunction() {
+      window.location.href = 'index.html';
+    }
+    imgHomeSetup.forEach((img) => { img.addEventListener('click', homeFunction); });
+  }
+  function mainFunction() {
+    const wrap = document.getElementById('nav-wrapper');
+    const openWrap = document.getElementById('wrapper');
+    const closeWrap = document.getElementById('close-wrap');
+    function wrapFunction() {
+      if (wrap.style.display === 'flex') {
+        wrap.style.display = 'none';
+      } else {
+        wrap.style.display = 'flex';
+      }
+    }
+    openWrap.addEventListener('click', wrapFunction);
+    closeWrap.addEventListener('click', wrapFunction);
+    const speakers = [
+      {
+        image: 'images/speaker1.png',
+        name: 'Kheng Newick',
+        title: 'Professor of Entrepreneurial Microbiology Studies at Harvard School',
+        description: 'Newick studies commons-based peer productions, and published his seminal book The Wealth of Network in 2006.',
+      },
+      {
+        image: 'images/speaker2.png',
+        name: 'Shaun Brien',
+        title: 'Director of Cancer Centre Nabi and a board member of BC Korea',
+        description: 'As the main venue for new studies production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, cancer and biology.',
+      },
+      {
+        image: 'images/speaker3.png',
+        name: 'Lila Moon',
+        title: 'Executive Director of the Science Journal',
+        description: 'Lila Moon is the Executive Director of Science Journal, the nonprofit organization that operates Science. Science is freely available in 290 languages and used by nearly a billion people around the world every month.',
+      },
+      {
+        image: 'images/speaker4.png',
+        name: 'Edmund Moon',
+        title: 'Executive Director of nature',
+        description: 'Edmund Moon is help to bring science to Asia and is an outspoken advocate for the research and digital commons. In 2012 he was inducted into the inaugural class of the Journal (JJS) science Hall of Fame',
+      },
+      {
+        image: 'images/speaker5.png',
+        name: 'Hollie Jackson',
+        title: 'President of Cell',
+        description: 'European integration, science and participation of youth trough online as her major condern, Redas report outlining potential changes to EU copyright law was approved by the Parliament in July.',
+      },
+      {
+        image: 'images/speaker6.png',
+        name: 'Renier Brentjens',
+        title: 'CEO of JABB, ex CEO of the Biotechnology foundation',
+        description: 'Ryan had been leading open-source projects at the Biotechnology Foundation such as the open source movement.',
+      },
+    ];
+    const body = document.getElementById('body');
+    const mainProgram = document.getElementById('main-program');
+    function speakersFunction() {
+      const section = document.createElement('section');
+      section.className = 'speakers';
+      body.appendChild(section);
+      mainProgram.insertAdjacentElement('afterend', section);
 
-  {
-    id: 'card-2',
-    title: 'Exhibition',
-    imageSource: './Images/lecture2.png',
-    desc: 'Get opportunities to network with CC affilates around the world and also after the summit',
-  },
+      const h2 = document.createElement('h2');
+      h2.innerHTML = 'Featured Speakers';
+      section.appendChild(h2);
 
-  {
-    id: 'card-3',
-    title: 'Forum',
-    imageSource: './Images/lecture3.png',
-    desc: 'Get opportunities to network with CC affilates around the world and also after the summit',
-  },
+      const line = document.createElement('h6');
+      line.innerHTML = '────';
+      line.className = 'line';
+      section.appendChild(line);
 
-  {
-    id: 'card-4',
-    title: 'Workshop',
-    imageSource: './Images/lecture4.png',
-    desc: 'Get opportunities to network with CC affilates around the world and also after the summit',
+      const div = document.createElement('div');
+      div.className = 'speakers-div';
+      section.appendChild(div);
 
-  },
+      const button = document.createElement('button');
+      button.id = 'more-mobile';
+      button.innerHTML = 'MORE <i class="fa-solid fa-chevron-down"></i>';
+      section.appendChild(button);
 
-  {
-    id: 'card-5',
-    title: 'IOT ignite',
-    imageSource: './Images/lecture5.png',
-    desc: 'Get opportunities to network with CC affilates around the world and also after the summit',
-  },
-];
+      for (let i = 0; i < speakers.length; i += 1) {
+        const divSpeaker = document.createElement('div');
+        divSpeaker.className = 'speaker-information';
+        div.appendChild(divSpeaker);
 
-const showCard = function (arr) {
-  arr.forEach((elem) => {
-    CardSection.insertAdjacentHTML(
-      'beforeend',
-      `
-      <article class="card" id="card-${elem.id}" tabindex="0">
-      <div tabindex="0" class="card-image"><img src="${elem.imageSource}" alt="${elem.title}-image"}></div>
-      <div class="card-menu">
-        <h4 class="title" tabindex="0">${elem.title}</h4>
-        <p class="caed-desc" id = "desc-${elem.id}">${elem.desc}</p>
-        </div>
-      </article>
-      `,
-    );
-  });
-};
+        const img = document.createElement('img');
+        img.src = speakers[i].image;
+        divSpeaker.appendChild(img);
 
-showCard(cards);
+        const divInfo = document.createElement('div');
+        divSpeaker.appendChild(divInfo);
+
+        const h3 = document.createElement('h3');
+        h3.innerHTML = speakers[i].name;
+        divInfo.appendChild(h3);
+
+        const p1 = document.createElement('p');
+        p1.innerHTML = speakers[i].title;
+        p1.className = 'speaker-footer';
+        divInfo.appendChild(p1);
+
+        const p2 = document.createElement('p');
+        p2.innerHTML = speakers[i].description;
+        p2.className = 'speaker-description';
+        divInfo.appendChild(p2);
+
+        if (i >= 2) {
+          divSpeaker.classList.add('button-speaker-action');
+        }
+      }
+    }
+    window.onload = speakersFunction();
+    const actionSpeaker = document.querySelectorAll('.button-speaker-action');
+    const partner = document.getElementById('partner');
+    const button = document.getElementById('more-mobile');
+    function moreFunction() {
+      if (partner.style.display === 'flex') {
+        partner.style.display = 'none';
+        button.innerHTML = 'MORE <i class="fa-solid fa-chevron-down">';
+        actionSpeaker.forEach((box) => { box.style.display = 'none'; });
+      } else {
+        partner.style.display = 'flex';
+        button.innerHTML = 'LESS <i class="fa-solid fa-chevron-up">';
+        actionSpeaker.forEach((box) => { box.style.display = 'flex'; });
+      }
+    }
+    button.addEventListener('click', moreFunction);
+    const imgHome = document.querySelectorAll('.img-home');
+    const imgHomeSetup = document.querySelectorAll('.img-home-setup');
+    function homeFunction() {
+      window.location.href = 'index.html';
+    }
+    imgHome.forEach((img) => { img.addEventListener('click', homeFunction); });
+    imgHomeSetup.forEach((img) => { img.addEventListener('click', homeFunction); });
+  }
+  const page = document.body.id;
+  if (page === 'body') {
+    mainFunction();
+  } else if (page === 'about') {
+    aboutFunction();
+  }
+});
